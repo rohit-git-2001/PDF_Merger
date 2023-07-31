82915dca6fd49dd30914db4f87e3b7320bab49b1
Rohit <rkp7133@gmail.com>
2023-07-31 17:03:33 +0530

    added more code

---
+++ merge.js
@@ -0,0 +1,16 @@
+const PDFMerger = require('pdf-merger-js');
+
+var merger = new PDFMerger();
+
+var mergerPdfs = async (p1, p2) => {
+    await merger.add('1.pdf');  //merge all pages. parameter is the path to file and filename.
+    await merger.add('2.pdf'); // merge only page 2
+
+    await merger.save('merged.pdf'); //save under given name and reset the internal document
+
+    // Export the merged PDF as a nodejs Buffer
+    // const mergedPdfBuffer = await merger.saveAsBuffer();
+    // fs.writeSync('merged.pdf', mergedPdfBuffer);
+}
+
+module.exports = { mergerPdfs };
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express')
+const multer = require('multer')
+const mergerPdfs = require('./merge')
+const upload = multer({ dest: 'uploads/' })
 const path = require('path')
 const app = express()
-const port = 3002
+const port = 3004
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "template/index.html"))
 })
 
-app.post('/merge', upload.array('photos', 12), function (req, res, next) {
-
+app.post('/merge', upload.array('pdfs', 2), function (req, res, next) {
+    console.log(req.files)
+    mergerPdfs(path.join(__dirname, req.files[0].path),
+        path.join(__dirname, req.files[1].path))
+              
+    res.redirect("http://localhost:3004/")
 })
 
 app.listen(port, () => {
